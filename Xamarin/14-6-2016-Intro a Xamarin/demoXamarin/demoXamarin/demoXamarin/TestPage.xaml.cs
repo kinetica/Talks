5653db8dec5495662ee927e6cc89de3a6a76e94a
sebastian <sebastian@kinetica-solutions.com>
2016-06-15 16:07:04 -0300

    Se sube el material de la charla de Introducción a Xamarin

---
+++ Xamarin/14-6-2016-Intro a Xamarin/demoXamarin/demoXamarin/demoXamarin/TestPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace demoXamarin
+{
+    public partial class TestPage : ContentPage
+    {
+        public TestPage()
+        {
+            InitializeComponent();
+            lblTexto.Text = "Hola";
+            btn.Clicked += (sender, args) =>
+            {
+                lblTexto.Text = "Cambio";
+                lblTexto.IsVisible = !lblTexto.IsVisible;
+            };
+        }
+    }
+}
